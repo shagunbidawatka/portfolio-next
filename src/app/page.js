e64477d59f771342c8422f47f0a000ef1fb7125a
Shagun Bidawatka <64001827+shagunbidawatka@users.noreply.github.com>
2024-05-08 22:59:54 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 "use client";
-import Image from "next/image";
-import { useEffect } from "react";
+
 import HomePage from "@/components/HomePage";
 import "../styles/globals.css";
 import About from "@/components/About";
@@ -8,10 +7,14 @@ import Experience from "@/components/Experience";
 import Contact from "@/components/Contact";
 import Blogs from "@/components/Blogs";
 import PortfolioSEO from "@/components/PortfolioSEO";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   return (
     <div className="relative z-0 bg-primary">
+      <SpeedInsights />
+      <Analytics />
       <PortfolioSEO />
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <HomePage />
